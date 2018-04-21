82b9807d4bc8b0f956cabc36a4d5017094927253
JessinRa <jessin.bdg@gmail.com>
2018-04-21 22:01:53 +0700

    image & object

--- Object.java
+++ Object.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+import javax.swing.*;
 
 public abstract class Object{
 
@@ -5,5 +7,28 @@ public abstract class Object{
 	protected double position_y;
 	protected String direction;
 
+    public double get_x(){
+        return position_x;
+    }
+
+	public double get_y(){
+        return position_y;
+    }
+
+	public String get_dir(){
+        return direction;
+    }	
+
+	public void set_x(double x){
+        position_x = x;
+    }
+	public void set_y(double y){
+        position_y = y;
+    }
+
+    public void set_dir(String dir){
+        direction = dir;
+    }
+    
     public abstract void draw();
 }
