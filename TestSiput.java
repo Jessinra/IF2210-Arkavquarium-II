1986c5a0834cfc2479977754ddf05f312dfa26e7
devipramita <devipram99@gmail.com>
2018-04-25 19:36:33 +0700

    ubah pet & testsiput

--- TestSiput.java
+++ TestSiput.java
@@ -1,49 +1,60 @@
 import junit.framework.TestCase;
-import org.junit.*;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.assertEquals;
+import org.junit.assertFalse;
 
 public class TestSiput extends TestCase {
     int sp = 1;
     private Siput s = new Siput();
 
+    @Before
+    public testSiput() {
+        LinkedList<Coin> C = new LinkedList<>();
+        C.add(10);
+        C.add(20);
+        C.add(30);
+        C.add(40);
+        C.add(50);
+    }
+
     @Test
-    public void testSpeed() {
+    public void testGetSpeed() {
         assertEquals(Constants.SIPUT_MOVEMENT_SPD, s.getSpeed());
         System.out.println(this.getName() + " >> success");
     }
 
     @Test
-    public void testGetSet() {
+    public void testSetSpeed() {
         s.setSpeed(2);
         assertEquals(2,s.getSpeed());
         System.out.println(this.getName() + " >> success");
     }
 
-    // @Test
-    // public void test_move(LinkedList<Coin> C) {
-    //     s.set_speed(3);
-    //     s.move();
-    //     assertEquals(3, s.get_speed());
-    //     System.out.println("success");
-    // }
+    @Test
+    public void testMove() {
+        int speedbefore = s.getSpeed();
+        s.move();
+        int speedafter = s.getSpeed();
+        assertFalse(speedbefore, speedafter);
+        System.out.println(this.getName() + " >> success");
+    }
 
-    // @Test
-    // public boolean test_find_coin(LinkedList<Coin> C) {
-    //     assertEquals(True, s.find_coin(C));
-    //     if (s.find_coin(C)) {
-    //         System.out.println("True");
-    //     } else {
-    //         System.out.println("False");
-    //     }
-    // }
+    @Test
+    public boolean testFindCoin() {
+        assertEquals(True, s.find_coin(C));
+        System.out.println(s.take_coin(C) + " >> success");
+    }
 
-    // @Test
-    // public int test_inRadius(LinkedList<Coin> C) {
-    //     assertEquals(1, s.inRadius(C));
-    // }
+    @Test
+    public int testInRadius() {
+        assertEquals(1, s.inRadius(C));
+        System.out.println(s.take_coin(C) + " >> success");
+    }
 
-    // @Test
-    // public int test_take_coin(LinkedList<Coin> C) {
-    //     assertEquals(1, s.take_coin(C));
-    //     System.out.println(s.take_coin(C));
-    // }
+    @Test
+    public int testTakeCoin() {
+        assertEquals(1, s.take_coin(C));
+        System.out.println(s.take_coin(C) + " >> success");
+    }
 }
